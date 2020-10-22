80e67679e8827deb52f8f628af9ad94482f914cb
LaurieSue <pink.bee.me@outlook.com>
2020-10-22 11:38:02 -0500

    Add project files.

---
+++ ConsoleAppStep221/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStep221
+{
+    class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Class1 has been instantiated.");
+            Console.ReadLine();
+        }
+
+
+
+        public void TakesTwoInt(int a, int b)
+        {
+            A = a;
+            B = b;
+
+            if ((a * 2 / 2) == a)
+            {
+                Console.WriteLine(b);
+                
+            }
+            else
+            {
+                Console.WriteLine("Have a Great Day!");
+            }
+
+            Console.ReadLine();
+        }
+        public int A { get; set; }
+           public int B { get; set; }
+        }
+    }
+
---
+++ ConsoleAppStep221/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStep221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem1 = new Class1();
+            problem1.TakesTwoInt(4,7);
+            problem1.TakesTwoInt(b:7,a:4);
+        }
+    }
+}
